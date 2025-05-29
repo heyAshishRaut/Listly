e8fad9007168791091a1afc6204d26bdd54e7b50
Ashish Raut <theashish32@gmail.com>
2025-05-29 10:25:46 +0530

    Checking mobile screen size

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,6 +1,6 @@
-import FullScreenClock01 from "./components/FullScreenClock01";
-import FullScreenClock02 from "./components/FullScreenClock02";
-import FullScreenClock03 from "./components/FullScreenClock03";
+// import FullScreenClock01 from "./components/FullScreenClock01";
+// import FullScreenClock02 from "./components/FullScreenClock02";
+// import FullScreenClock03 from "./components/FullScreenClock03";
 import Main from "./components/Main";
 
 function App() {
